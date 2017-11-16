a6e719de2c706cd21d8a6a75b28b6e30cef7c854
Amr <ameersorne@gmail.com>
2017-11-17 00:08:34 +0800

    Add files via upload

---
+++ Slay or Pray - Software Engineering/Slay or Pray - Software Engineering/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.scenedimin = New System.Windows.Forms.Timer(Me.components)
+        Me.storychange = New System.Windows.Forms.Timer(Me.components)
+        Me.title = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'scenedimin
+        '
+        Me.scenedimin.Interval = 50
+        '
+        'storychange
+        '
+        Me.storychange.Interval = 1000
+        '
+        'title
+        '
+        Me.title.Interval = 1000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Slay_or_Pray___Software_Engineering.My.Resources.Resources.img1
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(480, 360)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, -1)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(480, 360)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("MV Boli", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(109, 313)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(252, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Click anywhere to continue"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BackgroundImage = Global.Slay_or_Pray___Software_Engineering.My.Resources.Resources.bgstart
+        Me.ClientSize = New System.Drawing.Size(479, 358)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Opacity = 0R
+        Me.Text = "Slay or Prey"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents scenedimin As Timer
+    Friend WithEvents storychange As Timer
+    Friend WithEvents title As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Slay or Pray - Software Engineering/Slay or Pray - Software Engineering/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+
+    Dim counter As Integer = 0
+    Dim counter2 As Double = 0
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Me.Opacity = 0
+        PictureBox1.Image = My.Resources.img1
+        PictureBox2.Image = My.Resources.title
+        PictureBox2.Visible = False
+        scenedimin.Enabled = True
+
+
+    End Sub
+
+    Private Sub timerscene_Tick(sender As Object, e As EventArgs) Handles scenedimin.Tick
+
+        Me.Opacity += 0.1
+        If Me.Opacity = 1 Then
+            scenedimin.Stop()
+            storychange.Start()
+        End If
+
+    End Sub
+
+    Private Sub storychange_Tick(sender As Object, e As EventArgs) Handles storychange.Tick
+        counter += 1000
+
+
+        Select Case counter
+            Case 3000
+                PictureBox1.Image = My.Resources.img2
+            Case 6000
+                PictureBox1.Image = My.Resources.img3
+            Case 9000
+                PictureBox1.Image = My.Resources.img4
+            Case 12000
+                PictureBox1.Image = My.Resources.bgstart
+                PictureBox2.Visible = True
+                storychange.Stop()
+                title.Start()
+        End Select
+
+
+    End Sub
+
+    Private Sub title_Tick(sender As Object, e As EventArgs) Handles title.Tick
+        counter2 += 1
+        If (counter2 Mod 2 > 0) Then
+            Label1.Text = True
+
+        Else
+            Label1.Text = False
+        End If
+    End Sub
+End Class
